50c8a5aae9e6c7a2fec0e30c489b48680c3659b4
Theodoridis Johannes <theodoridis@hdm-stuttgart.de>
2024-11-01 15:22:16 +0100

    FIX bug when annotation is missing the 'iscrowd' key, now 0 by default

--- faster_coco_eval/core/cocoeval.py
+++ faster_coco_eval/core/cocoeval.py
@@ -270,7 +270,7 @@ class COCOeval:
         d_b = [d['boundary'] for d in dt]
 
         # compute iou between each dt and gt region
-        iscrowd = [int(o['iscrowd']) for o in gt]
+        iscrowd = [int(o.get("iscrowd", 0)) for o in gt]
         mask_ious = maskUtils.iou(d_m, g_m, iscrowd)
         boundary_ious = maskUtils.iou(d_b, g_b, iscrowd)
         # combine mask and boundary iou
